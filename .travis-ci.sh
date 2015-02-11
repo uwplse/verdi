dcee1e78c249d1d9a894c020c1dc451b79213e8d
Doug Woos <doug.woos@gmail.com>
2015-02-10 18:20:13 -0800

    TravisCI config

---
+++ .travis-ci.sh
@@ -0,0 +1,10 @@
+echo "yes" | sudo add-apt-repository ppa:avsm/ocaml41+opam12
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+export OPAMYES=1
+opam init --comp 4.01.0
+opam install ${OPAM_DEPENDS}
+eval `opam config env`
+opam repo add coq-8.5 https://github.com/coq/repo-8.5.git
+opam install coq
+make
