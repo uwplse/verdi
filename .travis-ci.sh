3ee0421a1c396ae050926e5b64d93a8a943fe8d4
Doug Woos <doug.woos@gmail.com>
2015-02-10 18:24:30 -0800

    Don't always recompile Ocaml

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,7 +2,7 @@ echo "yes" | sudo add-apt-repository ppa:avsm/ocaml41+opam12
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
-opam init --comp 4.01.0
+opam init
 opam install ${OPAM_DEPENDS}
 eval `opam config env`
 opam repo add coq-8.5 https://github.com/coq/repo-8.5.git
