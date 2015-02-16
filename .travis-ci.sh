ac9e3c49cf610dd04c1fd5e1378aebfe8d6f6819
James Wilcox <wilcoxjay@gmail.com>
2015-02-16 09:24:35 -0800

    update tarvis to point to 8.5beta1

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,9 +1,11 @@
 echo "yes" | sudo add-apt-repository ppa:avsm/ocaml41+opam12
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
-export OPAMYES=1
-opam init
-eval `opam config env`
-opam repo add coq-8.5 https://github.com/coq/repo-8.5.git
-opam install coq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra
+wget http://homes.cs.washington.edu/~jrw12/coq-8.5beta1-build.tgz
+tar xf coq-8.5beta1-build.tgz
+pushd coq-8.5beta1-build
+sudo make install
+popd
+make
+cd raft
 make
