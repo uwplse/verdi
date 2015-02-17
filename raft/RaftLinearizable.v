6f3bdea4b839273c5559d2bc165e82f2dcea5f1f
Doug Woos <doug.woos@gmail.com>
2015-02-16 23:12:58 -0800

    log_to_trace

--- raft/RaftLinearizable.v
+++ raft/RaftLinearizable.v
@@ -1,8 +1,12 @@
 Require Import List.
 Import ListNotations.
+Require Import Arith.
+Require Import Nat.
+Require Import Omega.
 
 Require Import Net.
 Require Import Util.
+Require Import VerdiTactics.
 
 Require Import Raft.
 Require Import Linearizability.
@@ -82,7 +86,28 @@ Section RaftLinearizable.
                                end)
       | _ :: xs => get_output xs k
     end.
-  
+
+
+
+  Fixpoint log_to_trace (env_o : key -> option output) (log : list entry) : list (IR key) :=
+    match log with
+      | [] => []
+      | mkEntry h client id index term input :: log' =>
+        (match env_o (client, id) with
+           | None => [IRI _ (client, id); IRU _ (client, id)]
+           | Some _ => [IRI _ (client, id); IRO _ (client, id)]
+         end) ++ log_to_trace env_o log'
+    end.
+
+  Lemma log_to_trace_good_trace :
+    forall env_o log,
+      good_trace _ (log_to_trace env_o log).
+  Proof.
+    intros.
+    induction log; simpl in *; auto.
+    - repeat break_match; simpl in *; constructor; auto.
+  Qed.
+
   Theorem raft_linearizable :
     forall failed net tr,
       step_f_star step_f_init (failed, net) tr ->
@@ -90,5 +115,7 @@ Section RaftLinearizable.
         equivalent _ (import tr) l /\
         exported (get_input tr) (get_output tr) l tr1 /\
         step_1_star init st tr1.
+  Proof.
+    intros. find_copy_apply_lem_hyp step_f_star_raft_intermediate_reachable.
   Admitted.
 End RaftLinearizable.
