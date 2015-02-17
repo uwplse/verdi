ad46d1df27d28f8c96ed3f4f4e10c4012aed0d8b
Doug Woos <doug.woos@gmail.com>
2015-02-17 13:20:05 -0800

    Work in progress on Raft linearizability

--- raft/RaftLinearizable.v
+++ raft/RaftLinearizable.v
@@ -9,6 +9,7 @@ Require Import Util.
 Require Import VerdiTactics.
 
 Require Import Raft.
+Require Import CommonTheorems.
 Require Import Linearizability.
 
 Section RaftLinearizable.
@@ -17,6 +18,11 @@ Section RaftLinearizable.
   Context {raft_params : RaftParams orig_base_params}.
 
   Definition key : Type := nat * nat.
+
+  Definition key_eq_dec : forall x y : key, {x = y} + {x <> y}.
+  Proof.
+    repeat decide equality.
+  Qed.
 
   Fixpoint import (tr : list (name * (raft_input + list raft_output)))
   : list (op key) :=
@@ -89,33 +95,78 @@ Section RaftLinearizable.
 
 
 
-  Fixpoint log_to_trace (env_o : key -> option output) (log : list entry) : list (IR key) :=
+  Fixpoint log_to_IR (env_o : key -> option output) (log : list entry) : list (IR key) :=
     match log with
       | [] => []
       | mkEntry h client id index term input :: log' =>
         (match env_o (client, id) with
            | None => [IRI _ (client, id); IRU _ (client, id)]
            | Some _ => [IRI _ (client, id); IRO _ (client, id)]
-         end) ++ log_to_trace env_o log'
+         end) ++ log_to_IR env_o log'
     end.
 
-  Lemma log_to_trace_good_trace :
+  Lemma log_to_IR_good_trace :
     forall env_o log,
-      good_trace _ (log_to_trace env_o log).
+      good_trace _ (log_to_IR env_o log).
   Proof.
     intros.
     induction log; simpl in *; auto.
     - repeat break_match; simpl in *; constructor; auto.
   Qed.
 
+  Fixpoint execute_log' (log : list entry) (st : data) (l : list (input * output))
+  : (list (input * output) * data) :=
+    match log with
+      | [] => (l, st)
+      | e :: log' => let '(o, st') := handler (eInput e) st in
+                    execute_log' log' st' (l ++ [(eInput e, o)])
+    end.
+
+  Definition execute_log (log : list entry) : (list (input * output) * data) :=
+    execute_log' log init [].
+
+  Definition input_correct (tr : list (name * (raft_input + list raft_output))) :=
+    NoDup (filterMap (fun x => match x with
+                                | (_, inl (ClientRequest client id _)) => Some (client, id)
+                                | _ => None
+                              end) tr).
+
+  Lemma execute_log_correct' :
+    forall log st,
+      step_1_star st (snd (execute_log' log st []))
+                  (fst (execute_log' log st [])).
+  Proof.
+  Admitted.
+
+  Lemma execute_log_correct :
+    forall log,
+      step_1_star init (snd (execute_log log))
+                  (fst (execute_log log)).
+  Proof.
+    intros. apply execute_log_correct'.
+  Qed.
+
   Theorem raft_linearizable :
     forall failed net tr,
+      input_correct tr ->
       step_f_star step_f_init (failed, net) tr ->
       exists l tr1 st,
         equivalent _ (import tr) l /\
         exported (get_input tr) (get_output tr) l tr1 /\
         step_1_star init st tr1.
   Proof.
-    intros. find_copy_apply_lem_hyp step_f_star_raft_intermediate_reachable.
-  Admitted.
+    intros.
+    exists (log_to_IR (get_output tr) (applied_entries (nwState net))).
+    exists (fst (execute_log (applied_entries (nwState net)))).
+    exists (snd (execute_log (applied_entries (nwState net)))).
+    intuition eauto using execute_log_correct.
+    - eapply equivalent_intro with (K_eq_dec := key_eq_dec); eauto using log_to_IR_good_trace.
+      + (* In O -> In IRO *)
+        admit.
+      + (* In IRO -> In O *)
+        admit.
+      + (* before preserved *)
+        admit.
+    - admit.
+  Qed.
 End RaftLinearizable.
