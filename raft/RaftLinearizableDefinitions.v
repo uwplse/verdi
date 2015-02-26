6794ce1dca2f6d1627bddd5f0bcb0073e03629cc
James Wilcox <wilcoxjay@gmail.com>
2015-02-25 19:59:11 -0800

    input_correct_snoc_inv

--- raft/RaftLinearizableDefinitions.v
+++ raft/RaftLinearizableDefinitions.v
@@ -20,4 +20,46 @@ Section RaftLinearizableDefinitions.
 
   Definition input_correct (tr : list (name * (raft_input + list raft_output))) : Prop :=
     NoDup (project_input_keys tr).
+
+  Lemma project_input_keys_app :
+    forall xs ys,
+      project_input_keys (xs ++ ys) = project_input_keys xs ++ project_input_keys ys.
+  Proof.
+    intros.
+    unfold project_input_keys.
+    now rewrite filterMap_app.
+  Qed.
+
+  Lemma project_input_keys_defn_nil :
+    project_input_keys [] = [].
+  Proof.
+    reflexivity.
+  Qed.
+
+  Lemma project_input_keys_defn_cons :
+    forall ev tr,
+      project_input_keys (ev :: tr) =
+      match ev with
+        | (_, inl (ClientRequest client id _)) => [ (client, id) ]
+        | _ => []
+      end ++ project_input_keys tr.
+  Proof.
+    unfold project_input_keys.
+    intros. simpl. repeat break_match; subst; try discriminate; auto.
+    find_inversion. auto.
+  Qed.
+
+  Lemma input_correct_snoc_inv :
+    forall tr ev,
+      input_correct (tr ++ [ev]) -> input_correct tr.
+  Proof.
+    unfold input_correct.
+    intros.
+    rewrite project_input_keys_app in *.
+    rewrite project_input_keys_defn_cons in *.
+    rewrite project_input_keys_defn_nil in *.
+    repeat break_match; subst; rewrite app_nil_r in *; auto.
+    find_apply_lem_hyp NoDup_append.
+    solve_by_inversion.
+  Qed.
 End RaftLinearizableDefinitions.
