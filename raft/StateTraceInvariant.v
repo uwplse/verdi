7aaddf8966d80b20b5f1752276d8a5c4b3f1d29b
James Wilcox <wilcoxjay@gmail.com>
2015-02-25 19:51:26 -0800

    fix StateTraceInvariant

--- raft/StateTraceInvariant.v
+++ raft/StateTraceInvariant.v
@@ -1,6 +1,8 @@
 Require Import List.
 Import ListNotations.
 
+Require Import FunctionalExtensionality.
+
 Require Import VerdiTactics.
 Require Import Net.
 
@@ -31,7 +33,7 @@ Section ST.
   Definition RT_input : Prop :=
     forall net failed tr h i out d l,
       step_f_star step_f_init (failed, net) tr ->
-      T tr ->
+      T (tr ++ [(h, inl i)]) ->
       R net tr ->
       input_handlers h i (nwState net h) = (out, d, l) ->
       R (mkNetwork (send_packets h l ++ nwPackets net)
@@ -39,6 +41,38 @@ Section ST.
         (tr ++ [(h, inl i); (h, inr out)]).
 
   Hypothesis H_RT_input : RT_input.
+
+  Definition RT_drop : Prop :=
+    forall net failed tr xs p ys,
+      step_f_star step_f_init (failed, net) tr ->
+      T tr ->
+      R net tr ->
+      nwPackets net = xs ++ p :: ys ->
+      R (mkNetwork (xs ++ ys) (nwState net))
+        tr.
+
+  Hypothesis H_RT_drop : RT_drop.
+
+  Definition RT_dup : Prop :=
+    forall net failed tr xs p ys,
+      step_f_star step_f_init (failed, net) tr ->
+      T tr ->
+      R net tr ->
+      nwPackets net = xs ++ p :: ys ->
+      R (mkNetwork (p :: xs ++ p :: ys) (nwState net))
+        tr.
+
+  Hypothesis H_RT_dup : RT_dup.
+
+  Definition RT_reboot : Prop :=
+    forall net failed tr h,
+      step_f_star step_f_init (failed, net) tr ->
+      T tr ->
+      R net tr ->
+      R (mkNetwork (nwPackets net) (update (nwState net) h (reboot (nwState net h))))
+        tr.
+
+  Hypothesis H_RT_reboot : RT_reboot.
 
   Lemma state_trace_invariant_invariant :
     forall failed net tr,
@@ -57,4 +91,35 @@ Section ST.
       end.
       + eapply H_RT_deliver with (failed := failed); eauto.
         apply refl_trans_n1_1n_trace; auto.
-      + eapply H_RT_input; eauto. SearchAbout (_ ++ _ :: _).
+      + eapply H_RT_input with (failed:=failed); eauto.
+        * apply refl_trans_n1_1n_trace; auto.
+        * eapply T_snoc_inv. rewrite app_ass. simpl. eauto.
+        * eapply IHrefl_trans_n1_trace; eauto.
+          eapply T_snoc_inv. eapply T_snoc_inv.
+          rewrite app_ass. simpl. eauto.
+      + rewrite app_nil_r in *.
+        eapply H_RT_drop with (failed:=failed); eauto.
+        apply refl_trans_n1_1n_trace. eauto.
+      + rewrite app_nil_r in *.
+        eapply H_RT_dup; eauto.
+        apply refl_trans_n1_1n_trace. eauto.
+      + rewrite app_nil_r in *. eauto.
+      + rewrite app_nil_r in *.
+        repeat concludes.
+        find_insterU. find_insterU.  conclude_using eauto.
+        eapply H_RT_reboot in IHrefl_trans_n1_trace; eauto.
+        * unfold update in *.
+          replace (fun nm : name =>
+                if name_eq_dec nm h
+                then reboot (nwState net0 nm)
+                else nwState net0 nm)
+                  with (fun nm : name =>
+                if name_eq_dec nm h
+                then reboot (nwState net0 h)
+                else nwState net0 nm );
+          eauto.
+          apply functional_extensionality.
+          intros. break_if; congruence.
+        * apply refl_trans_n1_1n_trace. eauto.
+  Qed.
+End ST.
