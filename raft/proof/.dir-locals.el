a29e4eb5c9170688313734bafda43f60b4ad84ca
James Wilcox <wilcoxjay@gmail.com>
2015-02-27 07:52:07 -0800

    introduce new raft/proof/ subdirectory

---
+++ raft/proof/.dir-locals.el
@@ -0,0 +1,17 @@
+((coq-mode . ((eval . (set (make-local-variable 'coq-prog-args)
+                            (list "-emacs-U" "-I"
+                                  (expand-file-name
+                                   ".."
+                                   (file-name-directory
+                                    (let ((d (dir-locals-find-file ".")))
+                                      (if (stringp d) d (car d))))
+                                   )
+                                  "-as" ""
+                                  "-I"
+                                  (expand-file-name
+                                   "../.."
+                                   (file-name-directory
+                                    (let ((d (dir-locals-find-file ".")))
+                                      (if (stringp d) d (car d))))
+                                   )
+                                  "-as" ""))))))
