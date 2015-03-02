5f2805da9f35382c3d3f6cb4310fa94fb5cd4249
Doug Woos <doug.woos@gmail.com>
2015-03-01 16:38:31 -0800

    Move some definitions to OutputImpliesAppliedInterface

--- raft/OutputImpliesAppliedInterface.v
+++ raft/OutputImpliesAppliedInterface.v
@@ -1,6 +1,9 @@
 Require Import List.
+Require Import Arith.
 
 Require Import Net.
+Require Import Util.
+Require Import VerdiTactics.
 Require Import Raft.
 
 Require Import CommonDefinitions.
@@ -18,6 +21,25 @@ Section OutputImpliesApplied.
   Definition in_output_list (os : list raft_output) :=
     exists o,
       In (ClientResponse client id o) os.
+
+  Definition is_client_response (out : raft_output) : bool :=
+    match out with
+      | ClientResponse c i _ => andb (beq_nat client c) (beq_nat id i)
+      | NotLeader _ _ => false
+    end.
+
+  Definition in_output_list_dec (os : list raft_output) :
+    {in_output_list os} + {~ in_output_list os}.
+  Proof.
+    unfold in_output_list.
+    destruct (find is_client_response os) eqn:?.
+    - find_apply_lem_hyp find_some. break_and.
+      unfold is_client_response in *. break_match; try discriminate.
+      subst. do_bool. break_and. do_bool. subst. left. eauto.
+    - right. intro. break_exists.
+      eapply find_none in H; eauto. unfold is_client_response in *.
+      find_apply_lem_hyp Bool.andb_false_elim. intuition; do_bool; congruence.
+  Qed.
 
   Definition in_output (tr : list (name * (raft_input + list raft_output))) : Prop :=
     exists os h,
--- raft/proof/OutputImpliesAppliedProof.v
+++ raft/proof/OutputImpliesAppliedProof.v
@@ -34,25 +34,6 @@ Section OutputImpliesApplied.
   Section inner.
   Variables client id : nat.
 
-  Definition is_client_response (out : raft_output) : bool :=
-    match out with
-      | ClientResponse c i _ => andb (beq_nat client c) (beq_nat id i)
-      | NotLeader _ _ => false
-    end.
-
-  Definition in_output_list_dec (os : list raft_output) :
-    {in_output_list client id os} + {~ in_output_list client id os}.
-  Proof.
-    unfold in_output_list.
-    destruct (find is_client_response os) eqn:?.
-    - find_apply_lem_hyp find_some. break_and.
-      unfold is_client_response in *. break_match; try discriminate.
-      subst. do_bool. break_and. do_bool. subst. left. eauto.
-    - right. intro. break_exists.
-      eapply find_none in H; eauto. unfold is_client_response in *.
-      find_apply_lem_hyp Bool.andb_false_elim. intuition; do_bool; congruence.
-  Qed.
-
   Theorem in_output_dec :
     forall tr : list (name * (raft_input + list raft_output)),
       {in_output client id tr} + {~ in_output client id tr}.
