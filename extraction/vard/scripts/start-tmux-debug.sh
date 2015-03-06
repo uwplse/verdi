46c193f03e79ee7fea94974e584eefe3071d93d2
Doug Woos <doug.woos@gmail.com>
2015-03-06 13:59:57 -0800

    Hack to leave tmux windows open if vard fails

--- extraction/vard/scripts/start-tmux-debug.sh
+++ extraction/vard/scripts/start-tmux-debug.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 tmux new-session -d -s 'vard-debug'
-tmux split-window -h './vard.native 1 -debug'
-tmux split-window -v -p 66 './vard.native 2 -debug'
-tmux split-window -v -p 50 './vard.native 3 -debug'
+tmux split-window -h
+tmux send-keys './vard.native 1 -debug' C-m
+tmux split-window -v -p 66
+tmux send-keys './vard.native 2 -debug' C-m
+tmux split-window -v -p 50
+tmux send-keys './vard.native 3 -debug' C-m
 tmux select-pane -L
 exec tmux attach
