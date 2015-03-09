5ceddd5316eab529f7905861e43e80ef9e381d4b
Doug Woos <doug.woos@gmail.com>
2015-03-09 16:08:27 -0700

    MatchIndexAllEntriesInterface wip

---
+++ raft/MatchIndexAllEntriesInterface.v
@@ -0,0 +1,30 @@
+Require Import List.
+
+Require Import VerdiTactics.
+Require Import Net.
+Require Import Util.
+Require Import Raft.
+Require Import RaftRefinementInterface.
+
+Section MatchIndexAllEntries.
+
+  Context {orig_base_params : BaseParams}.
+  Context {one_node_params : OneNodeParams orig_base_params}.
+  Context {raft_params : RaftParams orig_base_params}.
+
+  Definition match_index_all_entries net :=
+    forall e leader h,
+      eIndex e <= assoc_default name_eq_dec (matchIndex (snd (nwState net leader))) h 0 ->
+      In e (log (snd (nwState net leader))) ->
+      eTerm e = currentTerm (snd (nwState net leader)) ->
+      In (currentTerm (snd (nwState net leader)), e) (allEntries (fst (nwState net h))).
+
+  Class match_index_all_entries_interface : Prop :=
+    {
+      match_index_all_entries_invariant :
+        forall net,
+          refined_raft_intermediate_reachable net ->
+          match_index_all_entries net
+    }.
+  
+End MatchIndexAllEntries.
