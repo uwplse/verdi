44c1da23a174730b1ebf33b81658b139bd360e73
Doug Woos <doug.woos@gmail.com>
2015-03-17 16:45:03 -0700

    PRove that StateMachineSafety' -> StateMachineSafety

--- raft/proof/StateMachineSafetyProof.v
+++ raft/proof/StateMachineSafetyProof.v
@@ -4,15 +4,13 @@ Require Import VerdiTactics.
 Require Import Util.
 Require Import Net.
 
-Require Import CommonTheorems.
 Require Import Raft.
-Require Import RaftRefinementInterface.
-Require Import StateMachineSafetyInterface.
+Require Import CommonTheorems.
 Require Import CommitRecordedCommittedInterface.
-Require Import LeaderCompletenessInterface.
-Require Import AllEntriesLeaderLogsInterface.
-Require Import LogMatchingInterface.
-Require Import UniqueIndicesInterface.
+Require Import StateMachineSafetyInterface.
+Require Import StateMachineSafetyPrimeInterface.
+Require Import RaftRefinementInterface.
+
 
 Section StateMachineSafetyProof.
   Context {orig_base_params : BaseParams}.
@@ -21,120 +19,48 @@ Section StateMachineSafetyProof.
 
   Context {rri : raft_refinement_interface}.
   Context {crci : commit_recorded_committed_interface}.
-  Context {lci : leader_completeness_interface}.
-  Context {aelli : all_entries_leader_logs_interface}.
-  Context {lmi : log_matching_interface}.
-  Context {uii : unique_indices_interface}.
+  Context {smspi : state_machine_safety'interface}.
   
-  Definition lifted_state_machine_safety (net : network) : Prop :=
-    state_machine_safety (deghost net).
-
-  Theorem lift_log_matching :
-    forall net,
-      refined_raft_intermediate_reachable net ->
-      log_matching (deghost net).
+  Lemma exists_deghost_packet : 
+    forall net p,
+      In p (nwPackets (deghost net)) ->
+      exists (q : packet (params := raft_refined_multi_params)),
+        In q (nwPackets net) /\ p = deghost_packet q.
   Proof.
     intros.
-    eapply lift_prop; eauto using log_matching_invariant.
+    unfold deghost in *. simpl in *. do_in_map.
+    subst. eexists; eauto.
   Qed.
 
-  Theorem lift_entries_match :
-    forall net h h',
-      refined_raft_intermediate_reachable net ->
-      entries_match (log (snd (nwState net h))) (log (snd (nwState net h'))).
-  Proof.
-    intros.
-    find_apply_lem_hyp lift_log_matching.
-    unfold log_matching, log_matching_hosts in *. intuition.
-    unfold deghost in *. simpl in *.
-    repeat break_match; eauto.
-  Qed.
-
-  Theorem lift_UniqueIndices :
+  Lemma state_machine_safety_deghost :
     forall net,
       refined_raft_intermediate_reachable net ->
-      UniqueIndices (deghost net).
+      state_machine_safety (deghost net).
   Proof.
-    intros. eapply lift_prop; eauto using UniqueIndices_invariant.
+    intros. unfold state_machine_safety in *. intuition.
+    - unfold state_machine_safety_host. intros.
+      do 2 (find_apply_lem_hyp commit_recorded_committed_invariant; auto).
+      find_apply_lem_hyp state_machine_safety'_invariant.
+      unfold state_machine_safety' in *. intuition. eauto.
+    - unfold state_machine_safety_nw. intros.
+      find_apply_lem_hyp commit_recorded_committed_invariant; auto.
+      find_apply_lem_hyp state_machine_safety'_invariant.
+      unfold state_machine_safety', state_machine_safety_nw' in *. intuition.
+      find_apply_lem_hyp exists_deghost_packet. break_exists.
+      intuition. subst. simpl in *. repeat break_match. simpl in *.
+      subst. eapply H5 in H1; repeat find_rewrite; simpl; eauto.
   Qed.
 
-  Theorem lift_uniqueIndices_log :
-    forall net h,
-      refined_raft_intermediate_reachable net ->
-      uniqueIndices (log (snd (nwState net h))).
-  Proof.
-    intros.
-    find_apply_lem_hyp lift_UniqueIndices.
-    unfold UniqueIndices, uniqueIndices_host_invariant in *.
-    intuition.
-    unfold deghost in *. simpl in *. break_match; eauto.
-  Qed.
-  
-  Theorem state_machine_safety_host_invariant :
-    forall net,
-      refined_raft_intermediate_reachable net ->
-      state_machine_safety_host (deghost net).
-  Proof.
-    unfold state_machine_safety_host. intros.
-    find_apply_lem_hyp commit_recorded_committed_invariant; auto.
-    find_apply_lem_hyp commit_recorded_committed_invariant; auto.
-    unfold commit_recorded_committed in *.
-    find_copy_apply_lem_hyp leader_completeness_invariant.
-    unfold leader_completeness in *. intuition.
-    unfold committed in *. break_exists. intuition.
-    repeat match goal with
-             | [ H : directly_committed _ ?e |- _ ] =>
-               try match goal with
-                     | H' : context [ allEntries ] |- _ =>
-                       match type of H' with
-                         | context [ e ] => fail 3
-                       end
-                   end;
-                 let Hnew := fresh "H" in
-                 remember H as Hnew; unfold directly_committed in Hnew;
-                 match goal with
-                   | [ Heq : Hnew = H |- _ ] => clear Heq
-                 end
-           end.
-    break_exists. intuition.
-    assert (NoDup nodes) by eauto using all_fin_NoDup.
-    match goal with
-      | H : NoDup nodes, _ : NoDup ?l1, _ : NoDup ?l2 |- _ =>
-        eapply pigeon with (l := nodes) (sub1 := l1) (sub2 := l2) in H
-    end; eauto using all_fin_all, name_eq_dec, div2_correct.
-    break_exists.
-    intuition.
-    repeat find_apply_hyp_hyp.
-    do 2 find_apply_lem_hyp all_entries_leader_logs_invariant; auto.
-    intuition; try solve [break_exists; intuition; find_false; eauto].
-    match goal with
-      | [ _ : eIndex ?e <= eIndex ?x, _ : eIndex ?e' <= eIndex ?x',
-          _ : In ?x ?l |- ?e = ?e' ] =>
-        cut (In e l /\ In e' l)
-    end;
-      [intros; intuition;
-       eapply uniqueIndices_elim_eq;
-       eauto using lift_uniqueIndices_log|].
-    intuition;
-      match goal with
-        | _ : In ?e ?l, _ : eIndex ?e <= eIndex ?x, _ : In ?x ?l' |- In ?e ?l' =>
-          assert (entries_match l l') as Hem by eauto using lift_entries_match;
-            specialize (Hem x x e)
-      end; intuition.
-  Qed.
-      
   Theorem state_machine_safety_invariant :
     forall net,
       raft_intermediate_reachable net ->
       state_machine_safety net.
   Proof.
-    apply lower_prop.
     intros.
-    unfold state_machine_safety. split.
-    - auto using state_machine_safety_host_invariant.
-    - admit.
+    eapply lower_prop; intros; auto.
+    apply state_machine_safety_deghost; auto.
   Qed.
-
+      
   Instance smsi : state_machine_safety_interface.
   Proof.
     split.
