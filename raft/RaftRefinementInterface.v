9a2934f2b0982f7d4b7451e5f695ecfa759978a9
Doug Woos <doug.woos@gmail.com>
2015-04-28 12:11:01 -0700

    Revert "Change how votesWithLog is updated"
    This reverts commit 7f5ac8e7ede0982837e0777ff7081e45e8ff6a90.

--- raft/RaftRefinementInterface.v
+++ raft/RaftRefinementInterface.v
@@ -30,15 +30,15 @@ Section RaftRefinementInterface.
     let (st', r) :=
         handleRequestVote me (snd st) t candidateId lastLogIndex lastLogTerm
     in
-    match (votedFor (snd st), votedFor st') with
-      | (None, Some cid) =>
+    match (votedFor st') with
+      | Some cid =>
         {| votes := (currentTerm st', cid) :: votes (fst st) ;
            votesWithLog := (currentTerm st', cid, log st') :: votesWithLog (fst st) ;
            cronies := cronies (fst st) ;
            leaderLogs := leaderLogs (fst st) ;
            allEntries := allEntries (fst st)
         |}
-      | _ => fst st
+      | None => fst st
     end.
 
   Definition update_elections_data_requestVoteReply (me : name) (src : name) t voteGranted st :=
