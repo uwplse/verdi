d42f59b4a6e2dfcf1642645457dc9b5129dea021
Steve Anton <steve@stevefanton.com>
2015-05-11 20:09:51 -0700

    prove leaderlog candidate term sanity

--- raft-proofs/LeaderLogsTermSanityProof.v
+++ raft-proofs/LeaderLogsTermSanityProof.v
@@ -250,11 +250,151 @@ Section LeaderLogsTermSanity.
     - apply leaderLogs_currentTerm_sanity_reboot.
   Qed.
 
+
+  Ltac ctsc_start :=
+    red; unfold leaderLogs_currentTerm_sanity_candidate; simpl; intros.
+
+  Ltac ctsc_start_update :=
+    ctsc_start;
+    repeat find_higher_order_rewrite; update_destruct; rewrite_update; [|eauto].
+
+  Lemma leaderLogs_currentTerm_sanity_candidate_unchanged :
+    forall net st' h gd d ps',
+      leaderLogs_currentTerm_sanity_candidate net ->
+      (forall h' : Net.name, st' h' = update (nwState net) h (gd, d) h') ->
+      leaderLogs gd = leaderLogs (fst (nwState net h)) ->
+      currentTerm d >= currentTerm (snd (nwState net h)) ->
+      (type d = type (snd (nwState net h)) \/ type d <> Candidate) ->
+      leaderLogs_currentTerm_sanity_candidate {| nwPackets := ps'; nwState := st' |}.
+  Proof.
+    unfold leaderLogs_currentTerm_sanity_candidate. intros. find_higher_order_rewrite.
+    update_destruct; subst; rewrite_update; [|eauto].
+    simpl in *. repeat find_rewrite. intuition; find_apply_hyp_hyp; omega.
+  Qed.
+
+  Ltac ctsc_unchanged :=
+    red; intros; eapply leaderLogs_currentTerm_sanity_candidate_unchanged; subst; eauto.
+
+  Lemma leaderLogs_currentTerm_sanity_candidate_init :
+    refined_raft_net_invariant_init leaderLogs_currentTerm_sanity_candidate.
+  Proof.
+    ctsc_start. contradiction.
+  Qed.
+
+  Lemma leaderLogs_currentTerm_sanity_candidate_client_request :
+    refined_raft_net_invariant_client_request leaderLogs_currentTerm_sanity_candidate.
+  Proof.
+    ctsc_unchanged.
+    - apply update_elections_data_client_request_leaderLogs.
+    - find_apply_lem_hyp handleClientRequest_type; intuition.
+    - find_apply_lem_hyp handleClientRequest_type; intuition.
+  Qed.
+
+  Lemma leaderLogs_currentTerm_sanity_candidate_timeout :
+    refined_raft_net_invariant_timeout leaderLogs_currentTerm_sanity_candidate.
+  Proof.
+    ctsc_start_update. subst. simpl in *.
+    find_rewrite_lem update_elections_data_timeout_leaderLogs.
+    find_apply_lem_hyp handleTimeout_type_strong. intuition.
+    - repeat find_reverse_rewrite. find_apply_hyp_hyp. omega.
+    - find_apply_lem_hyp leaderLogs_currentTerm_sanity_invariant; auto.
+      repeat find_rewrite. intuition.
+  Qed.
+
+  Lemma leaderLogs_currentTerm_sanity_candidate_append_entries :
+    refined_raft_net_invariant_append_entries leaderLogs_currentTerm_sanity_candidate.
+  Proof.
+    ctsc_unchanged.
+    - apply update_elections_data_appendEntries_leaderLogs.
+    - find_apply_lem_hyp handleAppendEntries_type_term. intuition.
+    - find_apply_lem_hyp handleAppendEntries_type_term. intuition. right. congruence.
+  Qed.
+
+  Lemma leaderLogs_currentTerm_sanity_candidate_append_entries_reply :
+    refined_raft_net_invariant_append_entries_reply leaderLogs_currentTerm_sanity_candidate.
+  Proof.
+    ctsc_unchanged.
+    - find_apply_lem_hyp handleAppendEntriesReply_type_term. intuition.
+    - find_apply_lem_hyp handleAppendEntriesReply_type_term. intuition. right. congruence.
+  Qed.
+
+  Lemma leaderLogs_currentTerm_sanity_candidate_request_vote :
+    refined_raft_net_invariant_request_vote leaderLogs_currentTerm_sanity_candidate.
+  Proof.
+    ctsc_unchanged.
+    - apply leaderLogs_update_elections_data_requestVote.
+    - find_apply_lem_hyp handleRequestVote_type_term. intuition.
+    - find_apply_lem_hyp handleRequestVote_type_term. intuition. right. congruence.
+  Qed.
+
+  Lemma leaderLogs_currentTerm_sanity_candidate_request_vote_reply :
+    refined_raft_net_invariant_request_vote_reply leaderLogs_currentTerm_sanity_candidate.
+  Proof.
+    ctsc_start_update. simpl in *.
+    find_eapply_lem_hyp leaderLogs_update_elections_data_RVR; [|auto].
+    intuition.
+    - find_copy_apply_lem_hyp handleRequestVoteReply_type. intuition.
+      + find_rewrite. find_apply_hyp_hyp. omega.
+      + find_contradiction.
+      + find_contradiction.
+    - subst. unfold raft_data in *. simpl in *. find_contradiction.
+  Qed.
+
+  Lemma leaderLogs_currentTerm_sanity_candidate_do_leader :
+    refined_raft_net_invariant_do_leader leaderLogs_currentTerm_sanity_candidate.
+  Proof.
+    ctsc_unchanged.
+    - find_rewrite. auto.
+    - find_rewrite. simpl. find_apply_lem_hyp doLeader_type. intuition.
+    - find_rewrite. simpl. find_apply_lem_hyp doLeader_type. intuition.
+  Qed.
+
+  Lemma leaderLogs_currentTerm_sanity_candidate_do_generic_server :
+    refined_raft_net_invariant_do_generic_server leaderLogs_currentTerm_sanity_candidate.
+  Proof.
+    ctsc_unchanged.
+    - find_rewrite. auto.
+    - find_rewrite. simpl. find_apply_lem_hyp doGenericServer_type. intuition.
+    - find_rewrite. simpl. find_apply_lem_hyp doGenericServer_type. intuition.
+  Qed.
+
+  Lemma leaderLogs_currentTerm_sanity_candidate_state_same_packet_subset :
+    refined_raft_net_invariant_state_same_packet_subset leaderLogs_currentTerm_sanity_candidate.
+  Proof.
+    ctsc_start. repeat find_reverse_higher_order_rewrite. eauto.
+  Qed.
+
+  Lemma leaderLogs_currentTerm_sanity_candidate_reboot :
+    refined_raft_net_invariant_reboot leaderLogs_currentTerm_sanity_candidate.
+  Proof.
+    ctsc_start_update. subst. simpl in *. discriminate.
+  Qed.
+
+  Lemma leaderLogs_currentTerm_sanity_candidate_invariant :
+    forall net,
+      refined_raft_intermediate_reachable net ->
+      leaderLogs_currentTerm_sanity_candidate net.
+  Proof.
+    intros.
+    apply refined_raft_net_invariant; auto.
+    - apply leaderLogs_currentTerm_sanity_candidate_init.
+    - apply leaderLogs_currentTerm_sanity_candidate_client_request.
+    - apply leaderLogs_currentTerm_sanity_candidate_timeout.
+    - apply leaderLogs_currentTerm_sanity_candidate_append_entries.
+    - apply leaderLogs_currentTerm_sanity_candidate_append_entries_reply.
+    - apply leaderLogs_currentTerm_sanity_candidate_request_vote.
+    - apply leaderLogs_currentTerm_sanity_candidate_request_vote_reply.
+    - apply leaderLogs_currentTerm_sanity_candidate_do_leader.
+    - apply leaderLogs_currentTerm_sanity_candidate_do_generic_server.
+    - apply leaderLogs_currentTerm_sanity_candidate_state_same_packet_subset.
+    - apply leaderLogs_currentTerm_sanity_candidate_reboot.
+  Qed.
+
   Instance lltsi : leaderLogs_term_sanity_interface.
   Proof.
     split.
     - apply leaderLogs_term_sanity_invariant.
     - apply leaderLogs_currentTerm_sanity_invariant.
-    - admit.
+    - apply leaderLogs_currentTerm_sanity_candidate_invariant.
   Qed.
 End LeaderLogsTermSanity.
