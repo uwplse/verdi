469027f29d4a76ae5f7854d7b1faa682b3a85ea8
James Wilcox <wilcoxjay@gmail.com>
2015-06-23 15:52:25 -0700

    split aell into separate cases

--- raft-proofs/AllEntriesLeaderLogsProof.v
+++ raft-proofs/AllEntriesLeaderLogsProof.v
@@ -20,6 +20,46 @@ Section AllEntriesLeaderLogs.
 
   Context {rri : raft_refinement_interface}.
 
-  Instance aelli :  all_entries_leader_logs_interface.
+  Lemma leader_without_missing_entry_invariant :
+    forall net,
+      refined_raft_intermediate_reachable net ->
+      leader_without_missing_entry net.
   Admitted.
+
+  Lemma appendEntriesRequest_exists_leaderLog_invariant :
+    forall net,
+      refined_raft_intermediate_reachable net ->
+      appendEntriesRequest_exists_leaderLog net.
+  Admitted.
+
+  Lemma appendEntriesRequest_leaderLog_not_in_invariant :
+    forall net,
+      refined_raft_intermediate_reachable net ->
+      appendEntriesRequest_leaderLog_not_in net.
+  Admitted.
+
+  Lemma appendEntries_leader_invariant :
+    forall net,
+      refined_raft_intermediate_reachable net ->
+      appendEntries_leader net.
+  Admitted.
+
+  Lemma leaderLogs_leader_invariant :
+    forall net,
+      refined_raft_intermediate_reachable net ->
+      leaderLogs_leader net.
+  Admitted.
+
+  Instance aelli :  all_entries_leader_logs_interface.
+  Proof.
+    split.
+    intros.
+    red.
+    intuition.
+    - auto using leader_without_missing_entry_invariant.
+    - auto using appendEntriesRequest_exists_leaderLog_invariant.
+    - auto using appendEntriesRequest_leaderLog_not_in_invariant.
+    - auto using appendEntries_leader_invariant.
+    - auto using leaderLogs_leader_invariant.
+  Qed.
 End AllEntriesLeaderLogs.
