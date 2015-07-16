245baf877bf96e88edd3565f9ce13cc752ae744d
Doug Woos <doug.woos@gmail.com>
2015-07-15 19:58:00 -0700

    handleAppendEntries case of SML

--- raft-proofs/StateMachineCorrectProof.v
+++ raft-proofs/StateMachineCorrectProof.v
@@ -15,8 +15,14 @@ Local Arguments update {_} {_} {_} _ _ _ _ : simpl never.
 
 Require Import Raft.
 Require Import CommonDefinitions.
+Require Import SpecLemmas.
 Require Import CommonTheorems.
+Require Import SpecLemmas.
 Require Import SortedInterface.
+Require Import DecompositionWithPostState.
+Require Import MaxIndexSanityInterface.
+Require Import StateMachineSafetyInterface.
+Require Import LogMatchingInterface.
 
 Section StateMachineCorrect.
   Context {orig_base_params : BaseParams}.
@@ -24,6 +30,9 @@ Section StateMachineCorrect.
   Context {raft_params : RaftParams orig_base_params}.
 
   Context {si : sorted_interface}.
+  Context {misi : max_index_sanity_interface}.
+  Context {smsi : state_machine_safety_interface}.
+  Context {lmi : log_matching_interface}.
   
   Ltac update_destruct :=
     match goal with
@@ -749,8 +758,26 @@ Section StateMachineCorrect.
       deduplicate_log' l ks = deduplicate_log' l ks'.
   Admitted.
 
+  Ltac get_invariant_pre inv :=
+    match goal with
+      | H : raft_intermediate_reachable _ |- _=>
+        match (type of H) with
+          | context [mkNetwork] =>
+            fail 2
+        end || copy_apply inv H
+    end.
+
+  Ltac get_invariant_post inv :=
+    match goal with
+      | H : raft_intermediate_reachable _ |- _=>
+        match (type of H) with
+          | context [mkNetwork] =>
+            copy_apply inv H            
+        end
+    end.
+  
   Lemma state_machine_do_generic_server :
-    raft_net_invariant_do_generic_server state_machine_log.
+    raft_net_invariant_do_generic_server' state_machine_log.
   Proof.
     red. unfold state_machine_log in *. simpl in *. intros.
     find_higher_order_rewrite.
@@ -769,7 +796,7 @@ Section StateMachineCorrect.
                destruct (x <? y) eqn:?; auto
            end; do_bool;
            find_eapply_lem_hyp findGtIndex_necessary; omega).
-      find_copy_apply_lem_hyp logs_sorted_invariant; unfold logs_sorted in *; intuition.
+      get_invariant_pre logs_sorted_invariant; unfold logs_sorted in *; intuition.
       match goal with
         | |- context [commitIndex ?st] =>
           rewrite removeAfterIndex_app
@@ -799,6 +826,132 @@ Section StateMachineCorrect.
       rewrite filter_false. reflexivity.
   Qed.
 
+  Lemma handleAppendEntries_preserves_lastApplied_entries:
+    forall (p : packet) (net : network) (d : raft_data) 
+      (m : msg) (t : term) (n : name) (pli : logIndex) 
+      (plt : term) (es : list entry) (ci : logIndex) xs ys ps' st' e,
+      raft_intermediate_reachable net ->
+      raft_intermediate_reachable {| nwPackets := ps'; nwState := st' |} ->
+      (forall h : name, st' h = update (nwState net) (pDst p) d h) ->
+      (forall p' : packet,
+         In p' ps' ->
+         In p' (xs ++ ys) \/
+         p' = {| pSrc := pDst p; pDst := pSrc p; pBody := m |}) ->
+      handleAppendEntries (pDst p) (nwState net (pDst p)) t n pli plt es ci = (d, m) ->
+      nwPackets net = xs ++ p :: ys ->
+      pBody p = AppendEntries t n pli plt es ci ->
+      eIndex e <= lastApplied (nwState net (pDst p)) ->
+      In e (log (nwState net (pDst p))) ->
+      In e (log d).
+  Proof.
+    (* establish maxIndex guarantee in post state *)
+    intros.
+    get_invariant_post max_index_sanity_invariant.
+    unfold maxIndex_sanity in *; intuition.
+    unfold maxIndex_lastApplied in *; intuition.
+    match goal with
+      | H : forall _, lastApplied _ <= maxIndex _ |- _ =>
+        specialize (H (pDst p))
+    end. simpl in *. repeat find_higher_order_rewrite.
+    rewrite_update. repeat find_rewrite.
+
+    (* SMS *)
+    get_invariant_pre state_machine_safety_invariant.
+    unfold state_machine_safety in *. intuition.
+    match goal with
+      | H : state_machine_safety_nw _ |- _ =>
+        specialize (H (pDst p))
+    end.
+    find_copy_apply_lem_hyp handleAppendEntries_log.
+    intuition; repeat find_rewrite; eauto.
+    - match goal with
+        | _ : eIndex ?e <= lastApplied (nwState ?net ?h) |- _ =>
+          assert (commit_recorded net h e) by (unfold commit_recorded; eauto)
+      end.
+      get_invariant_pre log_matching_invariant.
+      unfold log_matching, log_matching_hosts in *. intuition.
+      copy_eapply_prop_hyp In In.
+      copy_eapply_prop_hyp pBody pBody; eauto.
+      intuition; try omega.
+      subst.
+      find_copy_apply_lem_hyp handleAppendEntries_same_lastApplied.
+      repeat find_rewrite. omega.
+    - match goal with
+        | _ : eIndex ?e <= lastApplied (nwState ?net ?h) |- _ =>
+          assert (commit_recorded net h e) by (unfold commit_recorded; eauto)
+      end.
+      get_invariant_pre log_matching_invariant.
+      unfold log_matching, log_matching_hosts in *. intuition.
+      copy_eapply_prop_hyp In In.
+      copy_eapply_prop_hyp pBody pBody; eauto.
+      intuition.
+      + apply in_app_iff. right.
+        apply removeAfterIndex_le_In; eauto; omega.
+      + apply in_app_iff. right.
+        apply removeAfterIndex_le_In; eauto; omega.
+      + match goal with
+          | _ : context [maxIndex (?a ++ ?b)] |- _ =>
+            pose proof maxIndex_app a b
+        end. intuition.
+        repeat find_rewrite.
+        find_copy_apply_lem_hyp handleAppendEntries_same_lastApplied.
+        repeat find_rewrite. omega.
+  Qed.
+    
+  Lemma state_machine_append_entries :
+    raft_net_invariant_append_entries' state_machine_log.
+  Proof.
+    red. unfold state_machine_log in *. simpl in *. intros.
+    find_higher_order_rewrite.
+    destruct_update; simpl in *; eauto.
+    erewrite handleAppendEntries_stateMachine; eauto.
+    find_higher_order_rewrite.
+    f_equal. f_equal. f_equal.
+    f_equal.
+    find_copy_apply_lem_hyp handleAppendEntries_same_lastApplied.
+    repeat find_rewrite.
+    get_invariant_pre logs_sorted_invariant.
+    get_invariant_post logs_sorted_invariant.
+    unfold logs_sorted in *. intuition.
+    apply removeAfterIndex_same_sufficient; auto.
+    - unfold logs_sorted_host in *. simpl in *.
+      match goal with
+        | H : forall h, sorted (log (st' h)) |- _ =>
+          specialize (H (pDst p))
+      end.
+      repeat find_higher_order_rewrite.
+      rewrite_update. auto.
+    - intros.
+      get_invariant_pre max_index_sanity_invariant.
+      unfold maxIndex_sanity, maxIndex_lastApplied in *. intuition.
+      enough (exists e', eIndex e' = eIndex e /\ In e' (log (nwState net (pDst p)))).
+      + break_exists. intuition.
+        find_copy_eapply_lem_hyp handleAppendEntries_preserves_lastApplied_entries;
+          repeat find_rewrite; eauto.
+        enough (x = e) by now subst.
+        eapply uniqueIndices_elim_eq; eauto.
+        apply sorted_uniqueIndices.
+        unfold logs_sorted_host in *. simpl in *.
+        match goal with
+        | H : forall h, sorted (log (st' h)) |- _ =>
+          specialize (H (pDst p))
+        end.
+        repeat find_higher_order_rewrite.
+        rewrite_update. auto.
+      + apply log_matching_invariant; auto.
+        intuition; eauto using le_trans.
+        enough (eIndex e > 0) by auto.
+        get_invariant_post log_matching_invariant.
+        unfold log_matching, log_matching_hosts in *. intuition.
+        match goal with
+          | H : context [eIndex _ > _] |- _ =>
+            eapply H with (h := (pDst p))
+        end.
+        simpl. repeat find_higher_order_rewrite.
+        rewrite_update. auto.
+    - intros. eauto using handleAppendEntries_preserves_lastApplied_entries.
+  Qed.
+  
   Theorem state_machine_correct_invariant :
     forall net,
       raft_intermediate_reachable net ->
