d3f86034f76d9653af8893a7568f85d0789316de
Doug Woos <doug.woos@gmail.com>
2015-07-16 20:54:47 -0700

    Easy win in AllEntriesLeaderLogs

--- raft-proofs/AllEntriesLeaderLogsProof.v
+++ raft-proofs/AllEntriesLeaderLogsProof.v
@@ -17,6 +17,7 @@ Require Import AppendEntriesRequestLeaderLogsInterface.
 Require Import OneLeaderLogPerTermInterface.
 Require Import LeaderLogsSortedInterface.
 Require Import RefinedLogMatchingLemmasInterface.
+Require Import AppendEntriesRequestsCameFromLeadersInterface.
 
 Require Import AllEntriesLeaderLogsInterface.
 
@@ -31,6 +32,7 @@ Section AllEntriesLeaderLogs.
   Context {ollpti : one_leaderLog_per_term_interface}.
   Context {llsi : leaderLogs_sorted_interface}.
   Context {rlmli : refined_log_matching_lemmas_interface}.
+  Context {aercfli : append_entries_came_from_leaders_interface}.
 
   Lemma leader_without_missing_entry_invariant :
     forall net,
@@ -42,7 +44,10 @@ Section AllEntriesLeaderLogs.
     forall net,
       refined_raft_intermediate_reachable net ->
       appendEntriesRequest_exists_leaderLog net.
-  Admitted.
+  Proof.
+    intros. unfold appendEntriesRequest_exists_leaderLog.
+    apply append_entries_came_from_leaders_invariant; auto.
+  Qed.
 
   Lemma appendEntriesRequest_leaderLog_not_in_invariant :
     forall net,
