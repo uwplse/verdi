f20eb7b55d9f844bab42b07cf86e3714ec21d15a
James Wilcox <jrw12@cs.washington.edu>
2016-02-02 12:20:29 -0800

    move to containers-based infrastructure

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,9 +1,5 @@
-echo "yes" | sudo add-apt-repository ppa:avsm/ocaml41+opam12
-sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra
-wget http://homes.cs.washington.edu/~jrw12/coq-8.5-build.tgz
-tar xf coq-8.5-build.tgz
-pushd coq-8.5
-sudo make install > /dev/null 2>&1
-popd
+wget 'http://homes.cs.washington.edu/~jrw12/coq-8.5-build-local.tgz'
+tar xf coq-8.5-build-local.tgz
+export PATH=$PWD/coq-8.5/bin:$PATH
+
 ./build.sh
