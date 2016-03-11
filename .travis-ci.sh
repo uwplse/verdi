c126b6ca87699d92ac076b88857e2f9f60b336ac
Zach Tatlock <ztatlock@cs.washington.edu>
2016-03-10 21:37:57 -0800

    provide mathcomp dep for travix

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,9 @@
 wget 'http://homes.cs.washington.edu/~jrw12/coq-8.5-build-local.tgz'
 tar xf coq-8.5-build-local.tgz
-export PATH=$PWD/coq-8.5/bin:$PATH
+export PATH="$PWD/coq-8.5/bin:$PATH"
+
+wget 'http://homes.cs.washington.edu/~ztatlock/mathcomp.tgz'
+tar xf mathcomp.tgz
+export mathcomp_PATH="$PWD/mathcomp/mathcomp"
 
 ./build.sh
