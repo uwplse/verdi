4c0917f927dca57f391b91b64e8da24f8fdfbd55
Zach Tatlock <ztatlock@cs.washington.edu>
2016-03-10 17:53:25 -0800

    beginnings of a dashboard

--- proofalytics/proofalytics.sh
+++ proofalytics/proofalytics.sh
@@ -5,27 +5,48 @@ set -e
 PADIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BUILD_TIMES="${PADIR}/build-times.csv"
 PROOF_SIZES="${PADIR}/proof-sizes.csv"
+INDEX="${PADIR}/index.html"
 
 function main {
   build-times
   proof-sizes
+  mkindex > "$INDEX"
 }
 
 # making this a function doesn't work w/ xargs :\
 csvsort="sort --field-separator=, --numeric-sort --reverse"
 
 function build-times {
+  echo "file,time" > "$BUILD_TIMES"
   find ${PADIR}/.. -name '*.buildtime' \
     | xargs ${csvsort} --key=2 \
-    > "$BUILD_TIMES"
+    >> "$BUILD_TIMES"
 }
 
 function proof-sizes {
+  echo "file,proof,lines,words,chars" \
+    > "$PROOF_SIZES"
   find ${PADIR}/.. -name '*.v' \
     | xargs ${PADIR}/proof-sizes.awk \
     | sed "s:${PADIR}/../::g" \
     | ${csvsort} --key=3 \
-    > "$PROOF_SIZES"
+    >> "$PROOF_SIZES"
+}
+
+function mkindex {
+  cat <<EOF
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Verdi Proofalytics</title>
+  <link rel='stylesheet' type='text/css' href='style.css'>
+</head>
+<body>
+  <h1>Verdi Proofalytics</h1>
+</body>
+</html>
+EOF
 }
 
 main
