063c92ced23823cc12869434a903b18fdef5cdf1
Doug Woos <doug.woos@gmail.com>
2016-03-16 16:47:20 -0700

    fetch StructTact in Travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,14 @@
+pushd ..
+
 wget 'http://homes.cs.washington.edu/~jrw12/coq-8.5-build-local.tgz'
 tar xf coq-8.5-build-local.tgz
 export PATH=$PWD/coq-8.5/bin:$PATH
+
+git clone 'http://github.com/uwplse/StructTact'
+pushd StructTact
+make
+popd
+
+popd
 
 ./build.sh
