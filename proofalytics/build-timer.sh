369c031ac8ed188e9e757f841f59b9d182ac1425
James Wilcox <jrw12@cs.washington.edu>
2016-03-16 21:23:45 -0700

    save and return exit status in build-timer.sh

--- proofalytics/build-timer.sh
+++ proofalytics/build-timer.sh
@@ -5,8 +5,10 @@ shift
 
 t0=$(date +"%s")
 "$cmd" "$@"
+status=$?
 t1=$(date +"%s")
 
 t=$(expr $t1 - $t0)
 for last; do true; done
 printf "%s,%d\n" "$last" "$t" > "${last}.buildtime"
+exit $status
