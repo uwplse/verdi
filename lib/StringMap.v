93dda91997cd291daf1c649642ae521c3b4e52e4
James Wilcox <jrw12@cs.washington.edu>
2016-04-18 11:47:47 -0700

    clean up maps, implement alist map

--- lib/StringMap.v
+++ lib/StringMap.v
@@ -8,7 +8,7 @@ Require Import Coqlib.
 Require Import StructTact.StructTactics.
 Require Export Maps.
 
-Module ITree(X: INDEXED_TYPE) (*<: TREE*).
+Module ITree(X: INDEXED_TYPE) <: TREE.
 
   Definition elt := X.t.
   Definition elt_eq := X.eq.
@@ -68,76 +68,6 @@ Module ITree(X: INDEXED_TYPE) (*<: TREE*).
     intros. unfold get, remove. rewrite PTree.grspec.
     repeat break_if; try find_apply_lem_hyp X.index_inj; congruence.
   Qed.
-
-  (*Definition beq: forall {A: Type}, (A -> A -> bool) -> t A -> t A -> bool := PTree.beq.
-  Theorem beq_correct:
-    forall (A: Type) (eqA: A -> A -> bool) (t1 t2: t A),
-    beq eqA t1 t2 = true <->
-    (forall (x: elt),
-     match get x t1, get x t2 with
-     | None, None => True
-     | Some y1, Some y2 => eqA y1 y2 = true
-     | _, _ => False
-    end).
-  Proof.
-  Admitted.
-
-  Definition map: forall (A B: Type), (elt -> A -> B) -> t A -> t B := PTree.map (fun i v => f (X.value i) v) m.
-  Hypothesis gmap:
-    forall (A B: Type) (f: elt -> A -> B) (i: elt) (m: t A),
-    get i (map f m) = option_map (f i) (get i m).
-
-  (** Same as [map], but the function does not receive the [elt] argument. *)
-  Variable map1:
-    forall (A B: Type), (A -> B) -> t A -> t B.
-  Hypothesis gmap1:
-    forall (A B: Type) (f: A -> B) (i: elt) (m: t A),
-    get i (map1 f m) = option_map f (get i m).
-
-  (** Applying a function pairwise to all data of two trees. *)
-  Variable combine:
-    forall (A B C: Type), (option A -> option B -> option C) -> t A -> t B -> t C.
-  Hypothesis gcombine:
-    forall (A B C: Type) (f: option A -> option B -> option C),
-    f None None = None ->
-    forall (m1: t A) (m2: t B) (i: elt),
-    get i (combine f m1 m2) = f (get i m1) (get i m2).
-
-  (** Enumerating the bindings of a tree. *)
-  Variable elements:
-    forall (A: Type), t A -> list (elt * A).
-  Hypothesis elements_correct:
-    forall (A: Type) (m: t A) (i: elt) (v: A),
-    get i m = Some v -> In (i, v) (elements m).
-  Hypothesis elements_complete:
-    forall (A: Type) (m: t A) (i: elt) (v: A),
-    In (i, v) (elements m) -> get i m = Some v.
-  Hypothesis elements_keys_norepet:
-    forall (A: Type) (m: t A), 
-    list_norepet (List.map (@fst elt A) (elements m)).
-  Hypothesis elements_extensional:
-    forall (A: Type) (m n: t A),
-    (forall i, get i m = get i n) ->
-    elements m = elements n.
-  Hypothesis elements_remove:
-    forall (A: Type) i v (m: t A),
-    get i m = Some v ->
-    exists l1 l2, elements m = l1 ++ (i,v) :: l2 /\ elements (remove i m) = l1 ++ l2.
-
-  (** Folding a function over all bindings of a tree. *)
-  Variable fold:
-    forall (A B: Type), (B -> elt -> A -> B) -> t A -> B -> B.
-  Hypothesis fold_spec:
-    forall (A B: Type) (f: B -> elt -> A -> B) (v: B) (m: t A),
-    fold f m v =
-    List.fold_left (fun a p => f a (fst p) (snd p)) (elements m) v.
-  (** Same as [fold], but the function does not receive the [elt] argument. *)
-  Variable fold1:
-    forall (A B: Type), (B -> A -> B) -> t A -> B -> B.
-  Hypothesis fold1_spec:
-    forall (A B: Type) (f: B -> A -> B) (v: B) (m: t A),
-    fold1 f m v =
-    List.fold_left (fun a p => f a (snd p)) (elements m) v.*)
 End ITree.
 
 Module IndexedString <: INDEXED_TYPE.
@@ -183,5 +113,11 @@ Module IndexedString <: INDEXED_TYPE.
   Qed.
 End IndexedString.
 
-Module StringTree := ITree(IndexedString).
-Module StringMap := IMap(IndexedString).
+Module LogTimeStringMap := ITree(IndexedString).
+
+Module EqString <: EQUALITY_TYPE.
+  Definition t := string.
+  Definition eq := string_dec.
+End EqString.
+
+Module LinearTimeStringMap := ETree(EqString).
