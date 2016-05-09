f8e31a9c82db7a91bef610adaf63e7594b628ea6
Ryan Hart Doenges <me@ryandoeng.es>
2016-05-09 14:36:24 -0700

    wip

--- systems/ChordProof.v
+++ systems/ChordProof.v
@@ -20,8 +20,11 @@ Section ChordProof.
   Notation failed_nodes := (failed_nodes addr payload data timeout).
   Notation sigma := (sigma addr payload data timeout).
 
+  Notation apply_handler_result := (apply_handler_result addr addr_eq_dec payload data timeout timeout_eq_dec).
+
   Notation step_dynamic := (step_dynamic addr addr_eq_dec payload data timeout timeout_eq_dec start_handler recv_handler timeout_handler client_payload can_be_client can_be_node).
 
+  Notation e_recv := (e_recv addr payload timeout).
   Notation e_timeout := (e_timeout addr payload timeout).
   Notation e_fail := (e_fail addr payload timeout).
 
@@ -121,5 +124,121 @@ Section ChordProof.
     ordered_ring gst /\
     connected_appendages gst /\
     base_not_skipped gst.
+
+  Lemma live_node_specificity : forall gst gst' ,
+      nodes gst = nodes gst' ->
+      failed_nodes gst = failed_nodes gst' ->
+      sigma gst = sigma gst' ->
+      live_node gst = live_node gst'.
+  Proof.
+    intuition.
+    unfold live_node.
+    rewrite H.
+    rewrite H0.
+    rewrite H1.
+    reflexivity.
+  Qed.
+
+  Lemma live_node_joined : forall gst h st,
+      live_node gst h ->
+      Some st = sigma gst h ->
+      joined st = true.
+  Proof.
+    intuition.
+    unfold live_node in H.
+    apply H.
+    symmetry.
+    apply H0.
+  Qed.
+
+  Lemma live_node_in_nodes : forall gst st h,
+      live_node gst h ->
+      Some st = sigma gst h ->
+      In h (nodes gst).
+  Proof.
+    unfold live_node.
+    intuition.
+    apply H with (st := st).
+    symmetry.
+    apply H0.
+  Qed.
+
+  Lemma live_node_not_in_failed_nodes : forall gst st h,
+      live_node gst h ->
+      Some st = sigma gst h ->
+      ~ In h (failed_nodes gst).
+  Proof.
+    unfold live_node.
+    intuition.
+    apply H with (st := st).
+    symmetry.
+    apply H0.
+    apply H1.
+  Qed.
+
+  Lemma live_node_characterization : forall gst gst' h st st',
+      live_node gst h ->
+      nodes gst = nodes gst' ->
+      failed_nodes gst = failed_nodes gst' ->
+      Some st = sigma gst h ->
+      Some st' = sigma gst' h ->
+      joined st = joined st' ->
+      live_node gst' h.
+  Proof.
+    intuition.
+    unfold live_node.
+    intuition.
+    - rewrite <- H3 in H5.
+      inversion H5.
+      rewrite <- H7.
+      apply live_node_joined with (st := st) in H.
+      * rewrite <- H4.
+        apply H.
+      * apply H2.
+    - apply live_node_in_nodes with (st := st) in H.
+      * rewrite <- H0.
+        apply H.
+      * apply H2.
+    - apply live_node_not_in_failed_nodes with (st := st) in H.
+      * apply H.
+        rewrite H1.
+        apply H6.
+      * apply H2.
+  Qed.
+
+  Lemma apply_handler_result_preserves_nodes : forall gst gst' h res e,
+      gst' = apply_handler_result h res e gst ->
+      nodes gst' = nodes gst.
+  Proof.
+    intuition.
+    
+
+  Lemma apply_handler_result_preserves_live_node_when_failed_node_preserved :
+    forall h h0 st st' gst gst' e ms cts nts,
+      live_node gst h ->
+      sigma gst h = Some st ->
+      sigma gst' h = Some st' ->
+      joined st = joined st' ->
+      gst' = apply_handler_result h0 (st', ms, cts, nts) e gst ->
+      live_node gst' h.
+  Proof.
+    intuition.
+    apply live_node_characterization with (gst := gst) (st := st) (st' := st').
+    - auto.
+    - apply apply_handler_result_preserves_nodes
+    unfold apply_handler_result in H3.
+    apply 
+    unfold live_node.
+
+    
+  Lemma live_node_preserved_by_recv_handler : forall gst h src st res msg gst' e,
+      live_node gst h ->
+      recv_handler src h st msg = res ->
+      gst' = apply_handler_result h res e gst' ->
+      live_node gst' h.
+  Proof.
+    intuition.
+    unfold apply_handler_result in H1.
+
 
 End ChordProof.
