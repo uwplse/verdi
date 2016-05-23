07d681008f46bc403fa389a0128fb03c0ee78af0
Keith <keith@the-simmons.net>
2016-05-23 02:15:05 -0700

    Progress on the serialized vst proof. Stuck on understanding the revert step lemma

--- systems/SerializedCorrect.v
+++ systems/SerializedCorrect.v
@@ -38,5 +38,86 @@ Section SerializedCorrect.
       (filteredMap revertPacket (nwPackets net))
       (fun h => match (deserialize (nwState net h)) with
              | Some (data, rest) => data
-             | None => 
+             | None => init_handlers h
              end).
+
+  Lemma Serialize_reversible_no_bin:
+    forall (A: Type) (serializer: Serializer A) (x: A),
+      deserialize (serialize x) = Some(x, []).
+  Proof.
+    intros.
+    rewrite <- Serialize_reversible with (x0 := x) (bin := []).
+    rewrite app_nil_r. reflexivity.
+  Qed.
+
+  Lemma Serialize_deserialize_init_handlers:
+    (fun h : name => match deserialize (serialize (init_handlers h)) with
+                 | Some (data, _) => data
+                 | None => init_handlers h
+                  end) = init_handlers.
+  Proof.
+    rewrite <- functional_extensionality with (f := (fun h : name => match deserialize (serialize (init_handlers h)) with
+                                                                 | Some (data, _) => data
+                                                                 | None => init_handlers h
+                                                                 end)); auto.
+    intros.
+    rewrite Serialize_reversible_no_bin with (x := (init_handlers x)).
+    auto.
+  Qed.
+
+  Lemma reachable_revert_step :
+    forall state new_state output,
+      reachable step_d step_m_init state ->
+      step_d state new_state output ->
+      (exists out, step_m (revertNetwork state) (revertNetwork new_state) output)
+      \/ revertNetwork state = revertNetwork newstate.
+  Proof.
+    intros.
+    match goal with H : step_d _ _ _ |- _ => invcs H end.
+    - unfold serialized_net_handlers in *.
+
+
+  Admitted.
+
+  Theorem reachable_revert :
+    true_in_reachable step_d step_m_init
+                      (fun st =>
+                         reachable step_m step_m_init (revertNetwork st)).
+  Proof.
+    apply true_in_reachable_reqs.
+    - unfold revertNetwork. simpl.
+      unfold reachable.
+      unfold serialized_init_handlers.
+      rewrite Serialize_deserialize_init_handlers.
+      exists [].
+      constructor.
+    - intros.
+      find_apply_lem_hyp reachable_revert_step; auto.
+      intuition.
+      + unfold reachable in *.
+        break_exists.
+        eexists.
+        apply refl_trans_n1_1n_trace.
+        econstructor; eauto using refl_trans_1n_n1_trace.
+      + find_rewrite. auto.
+  Qed.
+
+  Theorem true_in_reachable_transform :
+    forall P,
+      true_in_reachable step_m step_m_init P ->
+      true_in_reachable step_d step_m_init (fun net => P (revertNetwork net)).
+  Proof.
+    intros. find_apply_lem_hyp true_in_reachable_elim. intuition.
+    apply true_in_reachable_reqs.
+    - unfold step_m_init in *.
+      unfold revertNetwork.
+      simpl.
+      unfold serialized_init_handlers.
+      rewrite Serialize_deserialize_init_handlers.
+      assumption.
+    - intros. find_copy_apply_lem_hyp reachable_revert.
+      find_apply_lem_hyp reachable_revert_step; auto.
+      intuition.
+      + break_exists. eauto.
+      + repeat find_rewrite. auto.
+  Qed.
