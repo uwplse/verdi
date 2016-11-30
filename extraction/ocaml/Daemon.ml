69f031e0ac249f67e7626c4f0b201fba643aa55b
Karl Palmskog <palmskog@gmail.com>
2016-11-29 18:07:29 -0600

    introduce state and env into daemon

--- extraction/ocaml/Daemon.ml
+++ extraction/ocaml/Daemon.ml
@@ -1,51 +1,53 @@
-type task = 
+type ('env, 'state) task  = 
   {
     fd : Unix.file_descr ;
     mutable select_on : bool ;
     mutable wake_time : float option ;
-    mutable process_read : task -> bool * task list ;
-    mutable process_wake : task -> bool * task list ;
-    finalize : task -> unit ;
+    mutable process_read : ('env, 'state) task -> 'env -> 'state -> bool * ('env, 'state) task list * 'state ;
+    mutable process_wake : ('env, 'state) task -> 'env -> 'state -> bool * ('env, 'state) task list * 'state ;
+    finalize : ('env, 'state) task -> 'env -> 'state -> 'state ;
   }
 
-let handle_result hts task finish ts =
-  if finish then (Hashtbl.remove hts task.fd; task.finalize task);
-  List.iter (fun t -> Hashtbl.add hts t.fd t) ts
+let process f t hts env state =
+  let state = ref state in
+  let (finished, ts, state') = f t env !state in
+  state := state';
+  if finished then begin 
+    Hashtbl.remove hts t.fd; 
+    state := t.finalize t env !state 
+  end;
+  List.iter (fun t' -> Hashtbl.add hts t'.fd t') ts;
+  !state
 
-let rec eloop default_timeout hts =
+let rec eloop default_timeout hts env state =
+  let state = ref state in
   let start_time = Unix.gettimeofday () in
   let (select_fds, min_timeout) = 
-    Hashtbl.fold 
+    Hashtbl.fold
       (fun fd t (fds, timeout) ->
 	let fds' = if t.select_on then fd :: fds else fds in
-	let timeout' = 
-	  match t.wake_time with 
-	  | None -> timeout 
-	  | Some wake_time -> min timeout wake_time in
-        (fds', timeout'))
+	let timeout' =
+	  match t.wake_time with
+	  | None -> timeout
+	  | Some wake_time -> min timeout wake_time 
+	in (fds', timeout'))
       hts ([], default_timeout) in
   let (ready_fds, _, _) = Unix.select select_fds [] [] min_timeout in
-  List.iter 
+  List.iter
     (fun fd -> 
-      let t = Hashtbl.find hts fd in
-      let (finish, ts) = t.process_read t in
-      handle_result hts t finish ts) 
-    ready_fds;
+      let t = Hashtbl.find hts fd in 
+      state := process t.process_read t hts env !state) ready_fds;
   let elapsed_time = Unix.gettimeofday () -. start_time in
-  let wake_tasks = 
-    Hashtbl.fold 
-      (fun fd t ts -> 
+  let wake_tasks =
+    Hashtbl.fold
+      (fun fd t ts ->
 	match t.wake_time with
 	| None -> ts
-	| Some wake_time -> 
-	  if elapsed_time >= wake_time then 
+	| Some wake_time ->
+	  if elapsed_time >= wake_time then
 	    t :: ts
-	  else 
+	  else
 	    (t.wake_time <- Some (wake_time -. elapsed_time); ts))
       hts [] in
-  List.iter 
-    (fun t -> 
-      let (finish, ts) = t.process_wake t in
-      handle_result hts t finish ts)
-    wake_tasks;
-  eloop default_timeout hts
+  List.iter (fun t -> state := process t.process_wake t hts env !state) wake_tasks;
+  eloop default_timeout hts env !state
