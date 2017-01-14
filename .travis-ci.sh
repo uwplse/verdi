40015d20ef7fe21e07d158315698a503c3551a18
Karl Palmskog <palmskog@gmail.com>
2017-01-13 23:31:49 -0600

    only one opam install command to avoid upgrades

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,13 +4,13 @@ opam init --yes --no-setup
 eval $(opam config env)
 opam repo add coq-released https://coq.inria.fr/opam/released
 opam repo add distributedcomponents http://opam.distributedcomponents.net
-opam install coq.$COQ_VERSION coq-mathcomp-ssreflect.$SSREFLECT_VERSION StructTact InfSeqExt uuidm.0.9.6 --yes --verbose
+opam install coq.$COQ_VERSION coq-mathcomp-ssreflect.$SSREFLECT_VERSION \
+  StructTact InfSeqExt verdi-runtime --yes --verbose
 
 ./build.sh
 
 case $DOWNSTREAM in
 verdi-raft)
-  opam install verdi-runtime --yes --verbose
   pushd ..
     git clone -b $VERDI_RAFT_BRANCH 'http://github.com/uwplse/verdi-raft'
     pushd verdi-raft
