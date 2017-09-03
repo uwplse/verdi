a278c381ea56edceab99c60d67e2a92fa71847cf
Karl Palmskog <palmskog@gmail.com>
2017-09-02 22:52:47 -0500

    adapt to change of naming conventions in StructTact

--- extraction/ExtrOcamlFinInt.v
+++ extraction/ExtrOcamlFinInt.v
@@ -8,5 +8,5 @@ Extract Inlined Constant all_fin => "(fun n -> (Obj.magic (seq 0 n)))".
 
 Extract Inlined Constant fin_to_nat => "(fun _ n -> n)".
 
-Extract Inlined Constant fin_compare => "(fun _ n m -> if n = m then EQ else if n < m then LT else GT)".
-Extract Inlined Constant fin_comparison => "(fun _ n m -> if n = m then Eq else if n < m then Lt else Gt)".
+Extract Inlined Constant fin_compare_compat => "(fun _ n m -> if n = m then EQ else if n < m then LT else GT)".
+Extract Inlined Constant fin_compare => "(fun _ n m -> if n = m then Eq else if n < m then Lt else Gt)".
--- extraction/ExtrOcamlNatIntExt.v
+++ extraction/ExtrOcamlNatIntExt.v
@@ -7,3 +7,5 @@ Extract Inlined Constant Nat.min => "Pervasives.min".
 Extract Inlined Constant Nat.ltb => "(<)".
 
 Extract Inlined Constant nat_of_ascii => "Char.code".
+
+Extract Inlined Constant Nat.compare => "(fun n m -> if n = m then Eq else if n < m then Lt else Gt)".
