fd788dc749f167f21d643f51f12a9012f1a5e474
Karl Palmskog <palmskog@gmail.com>
2017-12-17 20:00:20 -0600

    new Travis attempt

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -6,8 +6,8 @@ eval $(opam config env)
 opam repo add coq-released https://coq.inria.fr/opam/released
 opam repo add distributedcomponents-dev http://opam-dev.distributedcomponents.net
 
-opam pin add coq $COQ_VERSION --yes --verbose
-opam pin add coq-mathcomp-ssreflect $SSREFLECT_VERSION --yes --verbose
+opam pin add coq ${COQ_VERSION} --yes --verbose
+opam pin add coq-mathcomp-ssreflect ${SSREFLECT_VERSION} --yes --verbose
 
 opam pin add verdi . --yes --verbose
 
