1be0b1603debed13000536bc99c33346c46b6d10
Michael Ernst <mernst@cs.washington.edu>
2018-01-04 12:49:38 -0800

    Try building under Docker

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,14 +1,5 @@
 set -ev
 
-opam init --yes --no-setup
-eval $(opam config env)
-
-opam repo add coq-released https://coq.inria.fr/opam/released
-opam repo add distributedcomponents-dev http://opam-dev.distributedcomponents.net
-
-opam pin add coq ${COQ_VERSION} --yes
-opam pin add coq-mathcomp-ssreflect ${SSREFLECT_VERSION} --yes
-
 opam pin add verdi . --yes --verbose
 
 case $DOWNSTREAM in
